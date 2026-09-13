c8647a60c1bc76995d2d877b18c2c07e4d8ff459
Bilal-Trigui <bilaltrigui06@gmail.com>
2026-09-13 19:43:10 -0400

    improved slave and master c files to make them more modular and readable

--- Master+Slave/master/master.c
+++ Master+Slave/master/master.c
@@ -1,108 +1,152 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/twi.h>
 
-int main(void){
-
-    uint8_t adcl_value;
-    uint8_t adch_value;
-    uint16_t adc_value;
-    
+void init_i2c(){
     //Configuring master mcu registers for 100kHz
     TWSR = 0x00;
     TWBR = 72;
     TWCR = (1 << TWEN);
+}
+
+void init_uart(){
+    UBRR0 = 103;
+    UCSR0B = (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void master_write_i2c(uint8_t addr, uint8_t command){
+
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to recieve start
+    }
+
+    if((TWSR & 0xF8) != TW_START) {
+        return;
+    }
+
+    TWDR = (addr << 1);
+
+    TWCR = (1 << TWINT) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to recieve start
+    }
+
+    if((TWSR & 0xF8) != TW_MT_SLA_ACK) {
+        return;
+    }
+
+    TWDR = command;
+
+    TWCR = (1 << TWINT) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to recieve start
+    }
+
+    if((TWSR & 0xF8) != TW_MT_DATA_ACK) {
+        return;
+    }
+
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+
+uint16_t master_recieve_i2c(uint8_t addr){
+
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to receive restart
+    }
+
+    if((TWSR & 0xF8) != TW_START) {
+        return;
+    }
+
+    TWDR = (addr << 1) | 1;
+
+    TWCR = (1 << TWINT) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to receive restart
+    }
+
+    if((TWSR & 0xF8) != TW_MR_SLA_ACK) {
+        return;
+    }
+
+    TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to receive restart
+    }
+
+    if((TWSR & 0xF8) != TW_MR_DATA_ACK) {
+        return;
+    }
+
+    uint8_t data_low = TWDR;
+
+    TWCR = (1 << TWINT) | (1 << TWEN);
+
+    while(!(TWCR & (1 << TWINT))){
+        //wait for slave to receive restart
+    }
+
+    if((TWSR & 0xF8) != TW_MR_DATA_NACK) {
+        return;
+    }
+
+    uint8_t data_high = TWDR;
+
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+
+    uint16_t data_adc = ((uint16_t) data_high << 8) | data_low;
+
+    return data_adc;
+}
+
+void uart_send_number(uint16_t value){
+    char buffer[5];
+    uint8_t i = 0;
+
+    if (value == 0)
+    {
+        while (!(UCSR0A & (1 << UDRE0))) {}
+        UDR0 = '0';
+        return;
+    }
+
+    while (value > 0)
+    {
+        buffer[i++] = (value % 10) + '0';
+        value /= 10;
+    }
+
+    while (i > 0)
+    {
+        while (!(UCSR0A & (1 << UDRE0))) {}
+        UDR0 = buffer[--i];
+    }
+}
+
+int main(void){
+
+    init_i2c();
+
+    init_uart();
 
     while(1){
-        TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
 
-        while(!(TWCR & (1 << TWINT))){
-            //wait for slave to recieve start
-        }
+        master_write_i2c(0x18, 0x24);
 
-        uint8_t status = TWSR & 0xF8;
+        uint16_t data = master_recieve_i2c(0x18);
 
-        if (status == 0x08){
+        uart_send_number(data);
 
-            TWDR = (0x18 << 1) | 0;  //address of the salve + write command
-            TWCR = (1 << TWINT) | (1 << TWEN);
-
-            while(!(TWCR & (1 << TWINT))){
-                //wait for slave to return address
-            }
-
-            status = TWSR & 0xF8;
-
-            if(status == 0x18){
-                //Slave returned address
-
-                TWDR = 0x24;
-                TWCR = (1 << TWINT) | (1 << TWEN);
-
-                while(!(TWCR & (1 << TWINT))){
-                    //wait for slave to transmit command
-                }
-
-                status = TWSR & 0xF8;
-
-                if(status == 0x28){
-                    //Slave recieved command
-
-                    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-
-                    while(!(TWCR & (1 << TWINT))){
-                        //wait for slave to receive restart
-                    }
-
-                    status = TWSR & 0xF8;
-
-                    if (status == 0x10){
-                        
-                        TWDR = (0x18 << 1) | 1; //Slave address + read command
-
-                        TWCR = (1 << TWINT) | (1 << TWEN);
-
-                        while(!(TWCR & (1 << TWINT))){
-                            //wait for slave to transmit address
-                        }
-
-                        status = TWSR & 0xF8;
-
-                        if (status == 0x40){
-                            //Slave recieved the read requests
-
-                            TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-
-                            while(!(TWCR & (1 << TWINT))){
-                                //wait for slave to send adc low byte
-                            }
-
-                            status = TWSR & 0xF8;
-
-                            if(status == 0x50) {
-                                //Slave recieved low byte for adc data
-
-                                adcl_value = TWDR;
-
-                                TWCR = (1 << TWINT) | (1 << TWEN);
-
-                                while(!(TWCR & (1 << TWINT))){
-                                    //wait for slave to send adc high byte
-                                }
-
-                                status = TWSR & 0xF8;
-
-                                if(status == 0x58) {
-                                    adch_value = TWDR;
-
-                                    adc_value = ((uint16_t)adch_value << 8) | adcl_value;
-                                }
-                            }
-
-                        }
-                    }
-
-                }
-            }
-        }
     }
+
 }
--- Master+Slave/slave/slave.c
+++ Master+Slave/slave/slave.c
@@ -1,124 +1,86 @@
 #include <avr/io.h>
 #include <stdint.h>
+#include <util/twi.h>
 
-// //converts the ADC Light Data Number from the LDR into a string that can be put into UDR0
-// void uart_send_number(uint16_t value)
-// {
-//     char buffer[5];
-//     uint8_t i = 0;
+void init_i2c(uint8_t addr){
+    TWAR = (addr << 1);
+    TWSR = 0;
+    TWCR = (1 << TWEN) | (1 << TWEA);
+}
 
-//     if (value == 0)
-//     {
-//         while (!(UCSR0A & (1 << UDRE0))) {}
-//         UDR0 = '0';
-//         return;
-//     }
+void init_adc(){;
+    ADMUX = 0x40;
+    ADCSRA = 0x87;
+}
 
-//     while (value > 0)
-//     {
-//         buffer[i++] = (value % 10) + '0';
-//         value /= 10;
-//     }
+uint16_t read_adc(){
+    uint8_t start_conversion = (1 << 6);
 
-//     while (i > 0)
-//     {
-//         while (!(UCSR0A & (1 << UDRE0))) {}
-//         UDR0 = buffer[--i];
-//     }
-// }
+    ADCSRA = ADCSRA | start_conversion;
+
+    while((ADCSRA & start_conversion) == start_conversion){
+        //do nothing (wait)
+        //run until we are done converting data
+    }
+
+    uint16_t = adcl_value = ADCL; //low byte of the ldr data
+    uint16_t adch_value = (ADCH << 8); //high byte of the ldr data
+
+    return (adch_value | adcl_value);
+}
+
+uint8_t receive_command(){
+   return TWDR;
+}
+
+void send_adc(uint16_t data){
+    uint8_t adc_high = data >> 8;
+    uint8_t adc_low = data & 0x00FF;
+
+    TWDR = adc_low;
+
+    TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+
+    while (!(TWCR & (1 << TWINT))){
+        //wait for master mcu to send acknowledgement
+    }
+
+    if((TWSR & 0xF8) != TW_ST_DATA_ACK){
+        return;
+    }
+
+    TWDR = adc_high;
+
+    TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
+
+    while (!(TWCR & (1 << TWINT))){
+        //wait for master mcu to send acknowledgement
+    }
+
+    if((TWSR & 0xF8) != TW_ST_DATA_NACK){
+        return;
+    }
+
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+
 
 int main(void){
+    init_i2c(0x18);
 
-    uint8_t adcl_value;
-    uint8_t adch_value;
+    init_adc();
 
-    uint8_t start_conversion = (1 << 6);
-    ADMUX = 0x40;
-    UBRR0 = 103;
-    UCSR0B = (1 << 3);
-    UCSR0C = (1 << 2) | (1 << 1);
-
-    TWAR = (0x18 << 1);
-    TWCR = (1 << TWEN) | (1 << TWEA);
+    uint8_t desired_command = 0x24;
 
     while(1){
+        
+        while(!(TWCR & (1 << TWINT))){}
+        
+        uint8_t command = receive_command();
 
-        while(!(TWCR & (1 << TWINT))) {
-            //wait for the master mcu to contact slave mcu
+        if(command == desired_command){
+            uint16_t data = read_adc();
+            send_adc(data);
         }
-
-        uint8_t status = TWSR & 0xF8;
-
-        if(status == 0x60){
-            //slave sends acknowlegdement to master
-
-            TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-        }
-
-        while(!(TWCR & (1 << TWINT))) {
-            //wait for the master mcu to send command to slave mcu
-        }
-
-        status = TWSR & 0xF8;
-
-        if(status == 0x80){
-
-            uint8_t command = TWDR;
-            TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-
-            if(command == 0x24){
-                //master mcu sent request to slave for LDR data
-
-                ADCSRA = 0x87;
-                ADCSRA = ADCSRA | start_conversion;
-
-                while((ADCSRA & start_conversion) == start_conversion){
-                    //do nothing (wait)
-                    //run until we are done converting data
-                }
-
-                adcl_value = ADCL; //low byte of the ldr data
-                adch_value = ADCH; //high byte of the ldr data
-
-                while (!(TWCR & (1 << TWINT))){
-                    //wait for master mcu to send read request
-                }
-
-                status = TWSR & 0xF8;
-
-                if(status == 0xA8){
-
-                    TWDR = adcl_value; //gives master the low bit of adc data
-
-                    TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-
-                    while (!(TWCR & (1 << TWINT))){
-                        //wait for master mcu to send acknowledgement
-                    }
-
-                    status = TWSR & 0xF8;
-
-                    if(status == 0xB8){
-                        //master recieved low bit and now needs high bit
-                        TWDR = adch_value;
-                        TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-                    }
-
-                    while (!(TWCR & (1 << TWINT))) {
-                        // wait for Master to finish
-                    }
-
-                    status = TWSR & 0xF8;
-
-                    if (status == 0xC0){
-                        //master is done reading data
-                        TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN);
-                    }
-                    
-                }
-            }
-        }
-
-        // uart_send_number(adc_value);
     }
 }
